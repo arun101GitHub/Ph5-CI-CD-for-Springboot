8b51627b5e7f90e99451bab499f6fa1e4a5c5308
arun101GitHub <thapaarun@gmail.com>
2021-05-03 15:29:03 -0700

    initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into customer(id,name,phone,email) values('100001','Tom Lance','967-123-2345','tlance@ibm.com');
+insert into customer(id,name,phone,email) values('100002','Shane Kramer','939-939-3934','skramer@hotmail.com');
+insert into customer(id,name,phone,email) values('100003','Arun Jung','897-654-1345','arun@yahoo.com');
+insert into customer(id,name,phone,email) values('100004','Jane Kirk','397-652-2345','jkirk@yahoo.com');
+insert into customer(id,name,phone,email) values('100005','Kane Bradford','102-354-2345','kbradford@gmail.org');
+insert into customer(id,name,phone,email) values('100006','Sean Arnold','557-=154-0345','sarnold@hotmail.com');
+insert into customer(id,name,phone,email) values('100007','Seema Raut','237-546-2135','seemar@msn.com');
